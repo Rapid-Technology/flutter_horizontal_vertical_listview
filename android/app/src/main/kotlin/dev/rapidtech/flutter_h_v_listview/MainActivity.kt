b7fe819aee3a297239ab2ed17c88cc41cafc8646
Yasin <yasinm289@gmail.com>
2021-07-13 17:19:20 +0430

    first commit

---
+++ android/app/src/main/kotlin/dev/rapidtech/flutter_h_v_listview/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.rapidtech.flutter_h_v_listview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Horizontal & Vertical ListView"),
+        backgroundColor: Colors.green[700],
+        centerTitle: true,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.all(10),
+                    child: Center(
+                      child: Text("Card $index"),
+                    ),
+                    color: Colors.green[700],
+                  ),
+                ),
+              ),
+              Flexible(
+                child: ListView.builder(
+                  itemCount: 15,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text("List $index"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
